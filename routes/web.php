f492de0acf582019ce38fcb90df04aef75c54820
Evan Szymkowicz <evan.szymkowicz@evanwolf.tech>
2020-02-24 11:57:21 -0500

    admin can and edit users

--- routes/web.php
+++ routes/web.php
@@ -25,18 +25,26 @@ Route::get('/admin', 'admin\AdminController@dashboard');
 // Admin Food Categories
 Route::get('/admin/food-categories', 'admin\FoodCategoriesController@index');
 Route::get('/admin/food-categories/create', 'admin\FoodCategoriesController@create');
-Route::get('/admin/food-categories/{id}/edit', 'admin\FoodCategoriesController@edit');
+Route::post('/admin/food-categories', 'admin\FoodCategoriesController@store');
+Route::get('/admin/food-categories/{id}/edit', 'admin\FoodCategoriesController@edit');\
+Route::put('/admin/food-categories/{id}', 'admin\FoodCategoriesController@update');
+Route::delete('/admin/food-categories/{id}/delete', 'admin\FoodCategoriesController@delete');
 
 // Admin Food Items
 Route::get('/admin/food-items', 'admin\FoodItemsController@index');
 Route::get('/admin/food-items/create', 'admin\FoodItemsController@create');
+Route::post('/admin/food-items', 'admin\FoodItemsController@store');
 Route::get('/admin/food-items/{id}/edit', 'admin\FoodItemsController@edit');
+Route::put('/admin/food-items/{id}', 'admin\FoodItemsController@update');
+Route::delete('/admin/food-items/{id}/delete', 'admin\FoodItemsController@delete');
 
 // Admin Users
 Route::get('/admin/users', 'admin\UsersController@index');
 Route::get('/admin/users/create', 'admin\UsersController@create');
 Route::post('/admin/users', 'admin\UsersController@store');
 Route::get('/admin/users/{id}/edit', 'admin\UsersController@edit');
+Route::put('/admin/users/{id}', 'admin\UsersController@update');
+Route::delete('/admin/users/{id}/delete', 'admin\UsersController@delete');
 
 // Admin Customers
 Route::get('/admin/offers-members', 'admin\CustomersController@allOffersMembers');
