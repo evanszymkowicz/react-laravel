bf3015e2e72eaa8f78958759dfc5b49103e07f2b
Evan Szymkowicz <evan.szymkowicz@evanwolf.tech>
2020-03-09 11:10:15 -0400

    updating the db

--- database/migrations/2020_03_09_142719_create_members_table.php
+++ database/migrations/2020_03_09_142719_create_members_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFoodCategoriesTable extends Migration
+class CreateMembersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateFoodCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('food_categories', function (Blueprint $table) {
+        Schema::create('members', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title');
-            $table->text('description');
-            $table->text('image_url');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateFoodCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('food_categories');
+        Schema::dropIfExists('members');
     }
 }
